de6c9ddc5ec181dfec9cd92d83f05daea3d4952c
Nandu Suresh <nandusuresh61@gmail.com>
2025-03-21 10:24:34 +0530

    random

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -14,6 +14,6 @@ const router = express.Router();
 router.post('/', authenticate, authorizeAdmin, upload.any(), addProduct);
 router.put('/:productId/variants/:variantId', authenticate, authorizeAdmin, upload.any(), editProduct);
 router.put('/:productId/variants/:variantId/toggle-status', authenticate, authorizeAdmin, toggleProductStatus);
-router.get('/', authenticate, authorizeAdmin, getAllProducts);
+router.get('/', getAllProducts);
 
 export default router;
