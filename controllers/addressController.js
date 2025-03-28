1b637a59df2d990d8a1dd42dc78a259b41fdebbc
Nandu Suresh <nandusuresh61@gmail.com>
2025-03-29 00:05:41 +0530

    random

--- controllers/addressController.js
+++ controllers/addressController.js
@@ -1,22 +1,18 @@
 import Address from '../models/addressModel.js';
 import asyncHandler from 'express-async-handler';
 
-// @desc    Get all addresses for a user
-// @route   GET /api/addresses
-// @access  Private
+
 export const getAllAddresses = asyncHandler(async (req, res) => {
   const addresses = await Address.find({ user: req.user._id });
   res.json(addresses);
 });
 
-// @desc    Add a new address
-// @route   POST /api/addresses
-// @access  Private
+
 export const addAddress = asyncHandler(async (req, res) => {
   const { type, name, street, city, state, pincode, phone, isDefault } = req.body;
 
   if (isDefault) {
-    // If new address is default, remove default from other addresses
+
     await Address.updateMany(
       { user: req.user._id },
       { $set: { isDefault: false } }
@@ -38,9 +34,7 @@ export const addAddress = asyncHandler(async (req, res) => {
   res.status(201).json(address);
 });
 
-// @desc    Edit an address
-// @route   PUT /api/addresses/:id
-// @access  Private
+
 export const editAddress = asyncHandler(async (req, res) => {
   const address = await Address.findOne({
     _id: req.params.id,
@@ -53,7 +47,6 @@ export const editAddress = asyncHandler(async (req, res) => {
   }
 
   if (req.body.isDefault) {
-    // If updating to default, remove default from other addresses
     await Address.updateMany(
       { user: req.user._id },
       { $set: { isDefault: false } }
@@ -69,9 +62,7 @@ export const editAddress = asyncHandler(async (req, res) => {
   res.json(updatedAddress);
 });
 
-// @desc    Delete an address
-// @route   DELETE /api/addresses/:id
-// @access  Private
+
 export const deleteAddress = asyncHandler(async (req, res) => {
   const address = await Address.findOne({
     _id: req.params.id,
@@ -88,17 +79,15 @@ export const deleteAddress = asyncHandler(async (req, res) => {
   res.json({ message: 'Address removed' });
 });
 
-// @desc    Set an address as default
-// @route   PUT /api/addresses/:id/default
-// @access  Private
+
 export const setDefaultAddress = asyncHandler(async (req, res) => {
-  // Remove default from all addresses
+
   await Address.updateMany(
     { user: req.user._id },
     { $set: { isDefault: false } }
   );
 
-  // Set new default address
+
   const address = await Address.findOneAndUpdate(
     { _id: req.params.id, user: req.user._id },
     { isDefault: true },
