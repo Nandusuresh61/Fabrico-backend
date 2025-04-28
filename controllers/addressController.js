cc3617be610c0c31ace8a267cc840b2b7049ff66
Nandu Suresh <nandusuresh61@gmail.com>
2025-04-28 10:09:49 +0530

    address validation added

--- controllers/addressController.js
+++ controllers/addressController.js
@@ -3,6 +3,37 @@ import asyncHandler from 'express-async-handler';
 import { HTTP_STATUS } from '../utils/httpStatus.js';
 
 
+const validateAddressData = (data) => {
+  const errors = [];
+  
+  if (!data.name || data.name.length < 4) {
+    errors.push('Name must be at least 4 characters long');
+  }
+
+  // Pincode validation (6 numbers)
+  if (!data.pincode || !/^\d{6}$/.test(data.pincode)) {
+    errors.push('Pincode must be 6 digits');
+  }
+
+
+  if (!data.phone || !/^\d{10}$/.test(data.phone)) {
+    errors.push('Phone number must be 10 digits');
+  }
+
+  if (!data.street) {
+    errors.push('Street address is required');
+  }
+  if (!data.city) {
+    errors.push('City is required');
+  }
+  if (!data.state) {
+    errors.push('State is required');
+  }
+
+  return errors;
+};
+
+
 export const getAllAddresses = asyncHandler(async (req, res) => {
   const addresses = await Address.find({ user: req.user._id });
   res.json(addresses);
@@ -12,8 +43,14 @@ export const getAllAddresses = asyncHandler(async (req, res) => {
 export const addAddress = asyncHandler(async (req, res) => {
   const { type, name, street, city, state, pincode, phone, isDefault } = req.body;
 
-  if (isDefault) {
+  // Validate address data
+  const validationErrors = validateAddressData({ name, street, city, state, pincode, phone });
+  if (validationErrors.length > 0) {
+    res.status(HTTP_STATUS.BAD_REQUEST);
+    throw new Error(validationErrors.join(', '));
+  }
 
+  if (isDefault) {
     await Address.updateMany(
       { user: req.user._id },
       { $set: { isDefault: false } }
@@ -35,7 +72,6 @@ export const addAddress = asyncHandler(async (req, res) => {
   res.status(HTTP_STATUS.CREATED).json(address);
 });
 
-
 export const editAddress = asyncHandler(async (req, res) => {
   const address = await Address.findOne({
     _id: req.params.id,
@@ -45,6 +81,13 @@ export const editAddress = asyncHandler(async (req, res) => {
   if (!address) {
     res.status(HTTP_STATUS.NOT_FONUD);
     throw new Error('Address not found');
+  }
+
+  // Validate address data
+  const validationErrors = validateAddressData(req.body);
+  if (validationErrors.length > 0) {
+    res.status(HTTP_STATUS.BAD_REQUEST);
+    throw new Error(validationErrors.join(', '));
   }
 
   if (req.body.isDefault) {
