721496458234068274968ea935ce0a2525d242ee
Nandu Suresh <nandusuresh61@gmail.com>
2025-05-06 14:23:10 +0530

    csrf changed samesite

--- utils/createToken.js
+++ utils/createToken.js
@@ -5,8 +5,8 @@ const generateToken = (res, userId) => {
 
     res.cookie('jwt', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV !== 'development',
-        sameSite: 'None',
+        secure: process.env.NODE_ENV === 'production',  // only true in prod
+        sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
         maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
     });
 
