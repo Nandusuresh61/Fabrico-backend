d21c059403aa7f6120246fec5169b2df84219caf
Nandu Suresh <nandusuresh61@gmail.com>
2025-05-06 10:08:59 +0530

    hosting bug

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { authenticate, authorizeAdmin } from '../middlewares/authMiddleware.js';
+import { authenticate, authorizeAdmin } from '../middlewares/authMiddleWare.js';
 import { getAllTransactions, getTransactionById } from '../controllers/transactionController.js';
 
 const router = express.Router();
